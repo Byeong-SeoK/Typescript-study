ec7f940964fd752c511e51a3f818737aae50e6ed
정병석 <qudtjr9849@gmail.com>
2022-08-24 14:13:14 +0900

    Typescript clone coding study 3

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import {init, exit} from "myPackage"
+import {init, exit} from "./myPackage"
 
 //init()
 //따로 myPackage.js에 대해 모듈화를 하지 않고
 //이 함수가 바로 동작하는 이유는 타입스크립트가 
 //코드를 보호하지 못하고 있기 때문이다.
 //그런 이유는 tsconfig.json에서 strict 모드로 설정하지 않았기 때문이다.
-
-init({
-    url: "true"
-})
-
-exit(1)
--- src/myPackage.js
+++ src/myPackage.js
@@ -1,7 +1,26 @@
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
+//위 코드 덕분에 타입스크립트는 자바스크립트 파일을 확인할 수 있게 된다.
+//뿐만 아니라 init함수의 config 인자가 객체이고 그 객체 안에는
+//boolean 타입의 debug, string 타입의 url이 property로 존재하고 있음을 알려준다.
+//또 cofig를 받는 함수의 return값이 void임을 알려준다.
+
 export function init(config){
     return true;
 }
 
+
+/**
+ * Exits the program
+ * @param {number} code 
+ * @returns number
+ */
 export function exit(code){
     return code+1;
 }
